59d8b3dd48e397f1bc3e0483efdf58962a835026
Bill Baran <bill.baran@gmail.com>
2018-07-11 16:04:23 -0600

    Update erc165.sol

--- erc165.sol
+++ erc165.sol
@@ -2,4 +2,6 @@ interface ERC165 {
     /// @return `true` if the contract implements `interfaceID` and
     ///  `interfaceID` is not 0xffffffff, `false` otherwise
     function supportsInterface(bytes4 interfaceID) external view returns (bool);
+    
+    /// ERC820 Provides an interface regisry
 }
